7354a6059b4b272bcd7ef06bc317268a45f8bf59
segabaser <139810447+segabaser@users.noreply.github.com>
2023-12-08 17:19:10 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,39 @@
+def main():
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+import random
+def generate_random_data():
+
+    return data
+    for item in data:
+
+    main()    main()
+    data = generate_random_data()
+
+    return data
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+    for item in data:
+def main():
+def main():
+import random
+    for item in data:
+
+    data = generate_random_data()
+    return data
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+        print(f"Random Number: {item}")
+def generate_random_data():
